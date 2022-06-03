d3e73787f9e9abe0a34a4efab5d1151bbcb18e74
zaryonogbor <ogborzaryon@gmail.com>
2022-06-03 12:51:31 -0700

    4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,25 @@
  * Return: Always (Success)
  */
 int main(void)
+
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
+
 	putchar(c);
+
 	}
+
 	for (c = 'A'; c <= 'Z'; c++)
 	{
+
 	putchar(c);
+
 	}
+
 	putchar('\n');
+
 	return (0);
 }
